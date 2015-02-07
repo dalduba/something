92b294cd7ac0877329c228f37aa48c9bb956dc4c
Dal Duba <dalduba@gmail.com>
2015-02-08 00:04:02 +0300

    template work

--- django/room/rooms/models.py
+++ django/room/rooms/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Room(models.Model):
+    name = models.CharField(max_length=80)
+    mapname = models.CharField(max_length=250)
+    maxplayer = models.IntegerField()
+    gametype = models.CharField(max_length=60)
--- django/room/rooms/views.py
+++ django/room/rooms/views.py
@@ -3,6 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import HttpResponse
+from django.template.loader import get_template
+from django.template import Template, Context
+from rooms.models import Room
 
 def hello(request):
-    return render(request, 'index.html')
+    rooms = Room.objects.all()
+    t = get_template('index.html')
+    c = Context({"rooms": rooms})
+    print(t.render(c))
+    return HttpResponse(t.render(c))
