55c50b43d8ae6c4a609b4cab99b11233e4da0e68
Dal Duba <dalduba@gmail.com>
2015-02-07 21:31:00 +0300

    index

--- django/room/room/settings.py
+++ django/room/room/settings.py
@@ -80,3 +80,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+TEMPLATE_DIRS = os.path.join(BASE_DIR, 'templates')
--- django/room/room/urls.py
+++ django/room/room/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import patterns, include, url
+from rooms.views import hello
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'room.views.home', name='home'),
+    url(r'^$', hello),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
