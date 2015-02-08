f39e31f0811d4b726ddacab3333d8d2b25296105
Dal Duba <dalduba@gmail.com>
2015-02-08 16:10:35 +0300

    form submit

--- django/room/rooms/views.py
+++ django/room/rooms/views.py
@@ -10,6 +10,9 @@ from rooms.models import Room
 def hello(request):
     rooms = Room.objects.all()
     t = get_template('index.html')
+    for room in rooms:
+        room.players = "%s/%s" % (2, room.maxplayer)
+        room.ping = 10
     c = Context({"rooms": rooms})
-    print(t.render(c))
+    print(request)
     return HttpResponse(t.render(c))
