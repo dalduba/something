186ab3402ddebbc50bfc1821f789d3e1fa41d44b
Dal Duba <dalduba@gmail.com>
2015-02-08 20:42:53 +0300

    ajax now playing update

--- django/room/room/urls.py
+++ django/room/room/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-from rooms.views import hello
+from rooms.views import hello, room_details
 
 from django.contrib import admin
 admin.autodiscover()
@@ -7,6 +7,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', hello),
+    url(r'^roomdetails$', room_details),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
--- django/room/rooms/views.py
+++ django/room/rooms/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from django.http import HttpResponse
+from django.http import StreamingHttpResponse, HttpResponse
 from django.template.loader import get_template
 from django.template import Template, Context
 from rooms.models import Room
+from django.views.decorators.csrf import ensure_csrf_cookie
+import json
 
 def hello(request):
     rooms = Room.objects.all()
@@ -16,3 +18,12 @@ def hello(request):
     c = Context({"rooms": rooms})
     print(request)
     return HttpResponse(t.render(c))
+
+@ensure_csrf_cookie
+def room_details(request):
+    if request.method == 'GET':
+        if request.GET['type'] == 'room_details':
+            print(request.GET['room_name'])
+    dumps = json.dumps([{"player":"player1","ping":"3"}, {"player":"player2","ping":"33"}])
+    print(dumps)
+    return StreamingHttpResponse(dumps)
